4a4e1571275452448d208dd1866f63cb7f51d872
Christopher-NguyenCS <69370834+Christopher-NguyenCS@users.noreply.github.com>
2024-06-25 23:25:06 -0400

    Fix UI

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-
+import { Inter } from 'next/font/google';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
